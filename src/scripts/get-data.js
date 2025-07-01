b7f61647221bd48776f7cfdd647136795cad419d
Nate Wright <natew@notthisway.com>
2025-07-01 12:11:35 +0100

    Sort events by date

--- src/scripts/get-data.js
+++ src/scripts/get-data.js
@@ -4,7 +4,7 @@ import { parse } from 'csv-parse/sync'
 import 'dotenv/config'
 
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID
-const URL = 'https://docs.google.com/spreadsheets/d/{{ID}}/gviz/tq?tqx=out:csv&sheet={{sheet_name}}'
+const URL = 'https://docs.google.com/spreadsheets/d/{{ID}}/gviz/tq?tqx=out:csv'
   .replace('{{ID}}', SPREADSHEET_ID)
 
 const COL_ID = 'ID'
@@ -24,6 +24,17 @@ const COL_COVERAGE2 = 'Coverage 2'
 const COL_COVERAGE3 = 'Coverage 3'
 
 const REGEX_DOMAIN = /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)/gi
+const REGEX_DATE = /^[0-9]{4}-[0-1][0-9]-[0-3][0-9]$/
+
+const validateRowData = row => {
+    if (!row[COL_DATE]) {
+      throw new Error(`Event missing date column:\n\n${JSON.stringify(row, null, 2)}`)
+    }
+    const dateMatches = row[COL_DATE].match(REGEX_DATE)
+    if (!dateMatches || !dateMatches.length) {
+      throw new Error(`Event date missing or invalid: ${row[COL_DATE]}\n\n${JSON.stringify(row, null, 2)}`)
+    }
+}
 
 const getCommaSeparatedList = str => str.split(',').map(s => s.trim())
 
@@ -35,23 +46,24 @@ const rows = await fetch(URL)
   })
 
 const events = rows
-  .map((event, i) => {
+  .map(row => {
+    validateRowData(row)
     return {
-      date: event[COL_DATE],
-      area: getCommaSeparatedList(event[COL_AREA]),
-      campaign: getCommaSeparatedList(event[COL_CAMPAIGN]),
-      sector: event[COL_SECTOR],
-      headline: event[COL_HEADLINE],
-      summary: event[COL_SUMMARY],
-      city: event[COL_CITY],
-      country: getCommaSeparatedList(event[COL_COUNTRY]),
-      region: getCommaSeparatedList(event[COL_REGION]),
-      target: getCommaSeparatedList(event[COL_TARGET]),
+      date: new Date(row[COL_DATE]),
+      area: getCommaSeparatedList(row[COL_AREA]),
+      campaign: getCommaSeparatedList(row[COL_CAMPAIGN]),
+      sector: row[COL_SECTOR],
+      headline: row[COL_HEADLINE],
+      summary: row[COL_SUMMARY],
+      city: row[COL_CITY],
+      country: getCommaSeparatedList(row[COL_COUNTRY]),
+      region: getCommaSeparatedList(row[COL_REGION]),
+      target: getCommaSeparatedList(row[COL_TARGET]),
       sources: [
-          event[COL_SOURCE],
-          event[COL_COVERAGE1],
-          event[COL_COVERAGE2],
-          event[COL_COVERAGE3],
+          row[COL_SOURCE],
+          row[COL_COVERAGE1],
+          row[COL_COVERAGE2],
+          row[COL_COVERAGE3],
         ]
         .filter(s => s)
         .map(url => {
@@ -65,9 +77,12 @@ const events = rows
         })
         ,
     }
-  }
-)
-
+  })
+  .sort((a, b) => a.date.getTime() - b.date.getTime())
+  .map(event => {
+    event.date = event.date.toISOString().split('T')[0]
+    return event
+  })
 
 try {
   fs.writeFileSync('./src/data/events.json', JSON.stringify(events, null, 2))
