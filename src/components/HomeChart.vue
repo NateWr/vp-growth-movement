b91fb237384fea405e2d9d1494a1c1ab2e5da9e3
Nate Wright <natew@notthisway.com>
2025-10-08 15:32:27 +0100

    Make chart lines full screen height on homepage

--- src/components/HomeChart.vue
+++ src/components/HomeChart.vue
@@ -29,6 +29,7 @@ const { width, BREAKPOINTS } = useViewportSize()
 
 <template>
   <Chart
+    class="homepage-chart-wrapper"
     :columns="columns"
     :datasets="datasets"
     :rows="rows"
@@ -36,3 +37,25 @@ const { width, BREAKPOINTS } = useViewportSize()
     :showAllYears="width >= BREAKPOINTS.LAPTOP_SM"
   />
 </template>
+
+<style>
+.homepage-chart-wrapper .chart-hexes {
+  align-self: end;
+  height: 25%;
+}
+@media (min-width: 767px) {
+  .homepage-chart-wrapper .chart-hexes {
+    height: 33%;
+  }
+}
+@media (min-width: 1280px) {
+  .homepage-chart-wrapper .chart-hexes {
+    height: 40%;
+  }
+}
+@media (min-width: 1920px) {
+  .homepage-chart-wrapper .chart-tick-label {
+    font-size: 1rem;
+  }
+}
+</style>
